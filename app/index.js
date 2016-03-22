b507dc41fbe99968c9f6b68067943441801ddd1b
natac13 <sean.campbell13@gmail.com>
2016-03-22 17:40:50 -0400

    Server Rendering Complete
    I got react rendering on the server and then getting taken over on the client.
    I have worked in passing the state from redux from the server to the client. At this point I have to make everything immutable once it gets to the client side. Seems wrong to do it this manual way.

--- app/index.js
+++ app/index.js
@@ -14,7 +14,16 @@ import configureStore from './store/configureStore';
 import configureRoutes from './routes/';
 const history = browserHistory;
 
-const store = configureStore();
+const initialState = window.__INITIAL_STATE__;
+// turn the immutable properties back to immutable
+initialState.counter = Immutable.fromJS(initialState.counter);
+
+console.log(window.__INITIAL_STATE__)
+console.log('initialState form the server');
+console.log(initialState);
+const store = configureStore(initialState);
+console.log('client state');
+console.log(store.getState())
 
 
 const rootElement = document.getElementById('root');
--- server/server.js
+++ server/server.js
@@ -8,10 +8,13 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import createLocation from 'history/lib/createLocation';
 import {
-  RoutingContext,
+  RouterContext,
   match,
   createMemoryHistory,
 } from 'react-router';
+
+import configureStore from '../app/store/configureStore';
+import configureRoutes from '../app/routes/';
 
 import { Provider } from 'react-redux';
 
@@ -21,6 +24,64 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config';
 const compiler = webpack(webpackConfig);
+
+function renderFullPage(html, initialState) {
+  return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name=viewport content="width=device-width, initial-scale=1">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>React Redux Server Side Attempt</title>
+      <!-- <link rel=stylesheet href="/build/style.css"> -->
+    </head>
+    <body>
+      <div id="root">${html}</div>
+      <script>
+        window.__INITIAL_STATE__ = ${initialState}
+      </script>
+      <script src="/build/bundle.js"></script>
+
+    </body>
+    </html>
+  `;
+}
+function handleRender(req, res) {
+  // create history
+  const history = createMemoryHistory();
+  // create new redux store instance every request
+  const store = configureStore();
+  // create routes
+  const routes = configureRoutes(history);
+  // create a location from the req.url
+  const location = createLocation(req.url);
+  // optional dispatch any actions, eg logging in a user.
+
+  match({ routes, location }, (error, redirectLocation, renderProps) => {
+    if (redirectLocation) {
+      res.redirect(301, redirectLocation.pathname + redirectLocation.search);
+    } else if (error) {
+      res.status(500).send(error.message);
+    } else if (renderProps === null) {
+      res.status(404).send('Not Found');
+    } else {
+      // pass the state from the store to the client
+      const initialState = JSON.stringify(store.getState());
+      console.log(initialState);
+      console.log('server state');
+      // create htmlString of components
+      const html = renderToString(
+        <Provider store={store}>
+          { <RouterContext { ...renderProps } /> }
+        </Provider>
+      );
+
+      res.send(renderFullPage(html, initialState));
+    }
+  });
+}
+
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const app = express();
@@ -33,23 +94,15 @@ if (isDevelopment) {
   app.use(webpackHotMiddleware(compiler));
 }
 
-// react and redux stuff I author
-import createRoutes from '../app/routes/';
-import configureStore from '../app/store/configureStore';
-
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
+app.get('/', handleRender);
 
 const port = process.env.PORT || 3013;
 
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`)
+  console.log(`Listening on port ${port}...`);
 });
-
-
 
 export default app;
