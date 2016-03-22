3f4d64e41ab8e2e9adfc142d62e8995f78a60f2b
natac13 <sean.campbell13@gmail.com>
2016-03-22 16:04:08 -0400

    I have added a counter component to display the count and 2 buttons for control.

--- app/actions/counterCreators.js
+++ app/actions/counterCreators.js
@@ -1,11 +1,11 @@
 import {
   COUNTER_SET,
-  COUNTER_INCREMNET,
+  COUNTER_INCREMENT,
   COUNTER_DECREMENT,
 } from '../constants/';
 
 import { createAction } from 'redux-actions';
 
 export const counterSet = createAction(COUNTER_SET);
-export const counterIncrement = createAction(COUNTER_INCREMNET);
+export const counterIncrement = createAction(COUNTER_INCREMENT);
 export const counterDecrement = createAction(COUNTER_DECREMENT);
--- app/components/Hello/Hello.js
+++ app/components/Hello/Hello.js
@@ -1,10 +1,16 @@
 import React, { PropTypes } from 'react';
 import { defaultProps } from 'recompose';
 
+import Counter from '../Counter/';
+
 function Hello(props) {
+  console.log(props)
   return (
     <div>
-      <h1>Hello, {props.name}</h1>
+      <h1 style={{ textAlign: 'center' }}>Hello, {props.name}</h1>
+      <div style={{ margin: 'auto', width: '75%' }}>
+        <Counter { ...props } />
+      </div>
     </div>
   );
 }
